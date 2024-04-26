37f8e693107c7032a09c6f6b03902d19fdf432dc
Roi Amiel <roi@Rois-MacBook-Pro.local>
2024-04-26 11:32:30 +0300

    now working

--- src/drone.sdc
+++ src/drone.sdc
@@ -2,4 +2,5 @@
 //All rights reserved.
 //File Title: Timing Constraints file
 //GOWIN Version: V1.9.9 Beta-6
-//Created Time: 2024-04-22 14:38:16
+//Created Time: 2024-04-23 17:15:42
+create_clock -name MicrosecondsUS -period 1000 -waveform {0 500} [get_ports {sys_clk}]
--- src/main.v
+++ src/main.v
@@ -1,25 +1,26 @@
 module PWMController(
     input sys_clk,          // clk input
     input sys_rst_n,        // reset input
-    input [31:0] pwm_width_ns,
+    input [31:0] pwm_width_us,
     output reg pwm_out
 );
 
 // ESC is a PWM with a frequency of 50Hz, i.e 50 Cycles in 1sec.
-// The FPGA board clock is (27MHz).
-// Therefore each 540,000 board clock cycles are 1 PWM cycle.
+// The sys_clk is 27MHz.
+// Therefore each 20 board clock cycles are 1 PWM cycle.
 `define CLOCK_CYCLES_IN_ESC_CYCLE 540000
-`define CLOCK_CYCLES_IN_NS 37
+`define CLOCK_CYCLES_IN_US 27
 
-reg [31:0] cycles_counter = 0; 
+reg [31:0] cycles_counter = 0;
 reg [31:0] phase_cycles_counter = 0;
-reg [31:0] phase_ns_counter = 0;
+reg [31:0] phase_us_counter = 0;
 
 always @(posedge sys_clk or negedge sys_rst_n) begin
     if (!sys_rst_n) begin
         cycles_counter <= 0;
         phase_cycles_counter <= 0;
-        phase_ns_counter <= 0;
+        phase_us_counter <= 0;
+        pwm_out <= 0;
     end
     else begin
         cycles_counter <= cycles_counter + 1;
@@ -27,24 +28,20 @@ always @(posedge sys_clk or negedge sys_rst_n) begin
 
         if (cycles_counter == `CLOCK_CYCLES_IN_ESC_CYCLE) begin
             cycles_counter <= 0;
-            phase_ns_counter <= 0;
+            phase_us_counter <= 0;
             phase_cycles_counter <= 0;
         end
 
-        if (phase_cycles_counter == `CLOCK_CYCLES_IN_NS) begin
+        if (phase_cycles_counter == `CLOCK_CYCLES_IN_US) begin
             phase_cycles_counter <= 0;
-            phase_ns_counter <= phase_ns_counter + 1;
+            phase_us_counter <= phase_us_counter + 1;
         end
+
+        if (phase_us_counter < pwm_width_us)
+            pwm_out <= 1;
+        else
+            pwm_out <= 0;
     end
-end
-
-always @(posedge sys_clk or negedge sys_rst_n) begin
-    if (!sys_rst_n)
-        pwm_out <= 0;
-    else if (phase_ns_counter < pwm_width_ns)
-        pwm_out <= 1;
-    else
-        pwm_out <= 0;
 end
 
 endmodule
@@ -58,7 +55,7 @@ module main(
 PWMController pwm_controller (
     .sys_clk(sys_clk),
     .sys_rst_n(sys_rst_n),
-    .pwm_width_ns(1000),
+    .pwm_width_us(1800),
     .pwm_out(pwm_out)
 );
 
