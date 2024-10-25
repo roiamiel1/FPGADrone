c2c9df3f0324195fab4fd68ff7758c0b16f999e8
Roi Amiel <roi@Rois-MacBook-Pro.local>
2024-10-26 00:21:07 +0300

    forwarding unit ignore zero register

--- src/hardware/ForwardingUnit.v
+++ src/hardware/ForwardingUnit.v
@@ -12,11 +12,11 @@ module ForwardingUnit (
     output [2:0] ALUDataIn2Mux_out   // Second MUX output.
 );
     assign ALUDataIn1Mux_out = 
-        ((ALUDataIn1RegAddr_in == EXMEM_WriteBackRegAddr) && EXMEM_WriteBackReg) ? `FOWARD_MUX_EXMEM_FORWARD :
-        (((ALUDataIn1RegAddr_in == MEMWB_WriteBackRegAddr) && MEMWB_WriteBackReg) ? `FOWARD_MUX_MEMWB_FORWARD : `FOWARD_MUX_NO_FORWARD);
+        ((ALUDataIn1RegAddr_in != 0 && ALUDataIn1RegAddr_in == EXMEM_WriteBackRegAddr) && EXMEM_WriteBackReg) ? `FOWARD_MUX_EXMEM_FORWARD :
+        (((ALUDataIn1RegAddr_in != 0 && ALUDataIn1RegAddr_in == MEMWB_WriteBackRegAddr) && MEMWB_WriteBackReg) ? `FOWARD_MUX_MEMWB_FORWARD : `FOWARD_MUX_NO_FORWARD);
 
     assign ALUDataIn2Mux_out = 
-        ((ALUDataIn2RegAddr_in == EXMEM_WriteBackRegAddr) && EXMEM_WriteBackReg) ? `FOWARD_MUX_EXMEM_FORWARD :
-        (((ALUDataIn2RegAddr_in == MEMWB_WriteBackRegAddr) && MEMWB_WriteBackReg) ? `FOWARD_MUX_MEMWB_FORWARD : `FOWARD_MUX_NO_FORWARD);
+        ((ALUDataIn2RegAddr_in != 0 && ALUDataIn2RegAddr_in == EXMEM_WriteBackRegAddr) && EXMEM_WriteBackReg) ? `FOWARD_MUX_EXMEM_FORWARD :
+        (((ALUDataIn2RegAddr_in != 0 && ALUDataIn2RegAddr_in == MEMWB_WriteBackRegAddr) && MEMWB_WriteBackReg) ? `FOWARD_MUX_MEMWB_FORWARD : `FOWARD_MUX_NO_FORWARD);
 
 endmodule
