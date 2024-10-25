43d087b27f931341c7f7c39066da4fa018fc1454
Roi Amiel <roi@Rois-MacBook-Pro.local>
2024-10-25 11:09:17 +0300

    fix ForwardingMux constants missing value bug

--- src/hardware/ForwardingUnit.v
+++ src/hardware/ForwardingUnit.v
@@ -9,12 +9,14 @@ module ForwardingUnit (
     input [4:0] MEMWB_WriteBackRegAddr,  // Which register address MEMWB planning to write to.
 
     output [2:0] ALUDataIn1Mux_out,  // First MUX output.
-    output [2:0] ALUDataIn2Mux_out  // Second MUX output.
+    output [2:0] ALUDataIn2Mux_out   // Second MUX output.
 );
-    assign ALUDataIn1Mux_out = (ALUDataIn1RegAddr_in == EXMEM_WriteBackRegAddr & EXMEM_WriteBackReg) ? `FOWARD_MUX_EXMEM_FORWARD :
-        (ALUDataIn1RegAddr_in == MEMWB_WriteBackRegAddr & MEMWB_WriteBackReg) ? `FOWARD_MUX_MEMWB_FORWARD : `FOWARD_MUX_NO_FORWARD;
+    assign ALUDataIn1Mux_out = 
+        ((ALUDataIn1RegAddr_in == EXMEM_WriteBackRegAddr) && EXMEM_WriteBackReg) ? `FOWARD_MUX_EXMEM_FORWARD :
+        (((ALUDataIn1RegAddr_in == MEMWB_WriteBackRegAddr) && MEMWB_WriteBackReg) ? `FOWARD_MUX_MEMWB_FORWARD : `FOWARD_MUX_NO_FORWARD);
 
-    assign ALUDataIn2Mux_out = (ALUDataIn2RegAddr_in == EXMEM_WriteBackRegAddr & EXMEM_WriteBackReg) ? `FOWARD_MUX_EXMEM_FORWARD :
-        (ALUDataIn2RegAddr_in == MEMWB_WriteBackRegAddr & MEMWB_WriteBackReg) ? `FOWARD_MUX_MEMWB_FORWARD : `FOWARD_MUX_NO_FORWARD;
+    assign ALUDataIn2Mux_out = 
+        ((ALUDataIn2RegAddr_in == EXMEM_WriteBackRegAddr) && EXMEM_WriteBackReg) ? `FOWARD_MUX_EXMEM_FORWARD :
+        (((ALUDataIn2RegAddr_in == MEMWB_WriteBackRegAddr) && MEMWB_WriteBackReg) ? `FOWARD_MUX_MEMWB_FORWARD : `FOWARD_MUX_NO_FORWARD);
 
 endmodule
