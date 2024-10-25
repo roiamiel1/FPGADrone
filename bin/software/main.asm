ff5029c79523278f70a622e139ea00ae7af1c7ae
Roi Amiel <roi@Rois-MacBook-Pro.local>
2024-10-25 13:42:25 +0300

    add binaries of mips c

--- bin/software/main.asm
+++ bin/software/main.asm
@@ -4,7 +4,7 @@
 	.nan	legacy
 	.module	fp=32
 	.module	nooddspreg
-	.module	arch=r2000
+	.module	arch=mips1
 	.abicalls
 	.text
 	.align	2
@@ -14,60 +14,24 @@
 	.ent	main
 	.type	main, @function
 main:
-	.frame	$fp,32,$31		# vars= 16, regs= 1/0, args= 0, gp= 8
+	.frame	$fp,24,$31		# vars= 8, regs= 1/0, args= 0, gp= 8
 	.mask	0x40000000,-4
 	.fmask	0x00000000,0
 	.set	noreorder
 	.set	nomacro
-	addiu	$sp,$sp,-32
-	sw	$fp,28($sp)
+	addiu	$sp,$sp,-24
+	sw	$fp,20($sp)
 	move	$fp,$sp
-	li	$2,5			# 0x5
-	sw	$2,16($fp)
-	lui	$2,%hi($LC0)
-	lwc1	$f0,%lo($LC0)($2)
-	nop
-	swc1	$f0,8($fp)
-	lw	$2,16($fp)
-	nop
-	mtc1	$2,$f0
-	nop
-	cvt.s.w	$f0,$f0
-	lwc1	$f2,8($fp)
-	nop
-	add.s	$f0,$f2,$f0
-	swc1	$f0,8($fp)
-	sw	$0,12($fp)
-	.option	pic0
-	b	$L2
-	nop
-
-	.option	pic2
-$L3:
-	lw	$2,16($fp)
-	nop
-	mtc1	$2,$f0
-	nop
-	cvt.s.w	$f0,$f0
-	lwc1	$f2,8($fp)
-	nop
-	add.s	$f0,$f2,$f0
-	swc1	$f0,8($fp)
-	lw	$2,12($fp)
-	nop
-	addiu	$2,$2,1
-	sw	$2,12($fp)
-$L2:
-	lw	$2,12($fp)
-	nop
-	slt	$2,$2,10
-	bne	$2,$0,$L3
-	nop
-
+	li	$2,65536			# 0x10000
+	ori	$2,$2,0x2345
+	sw	$2,8($fp)
+	lw	$2,8($fp)
+	li	$3,4			# 0x4
+	sb	$3,0($2)
 	move	$2,$0
 	move	$sp,$fp
-	lw	$fp,28($sp)
-	addiu	$sp,$sp,32
+	lw	$fp,20($sp)
+	addiu	$sp,$sp,24
 	jr	$31
 	nop
 
@@ -75,9 +39,5 @@ $L2:
 	.set	reorder
 	.end	main
 	.size	main, .-main
-	.rdata
-	.align	2
-$LC0:
-	.word	1089680179
 	.ident	"GCC: (Ubuntu 12.3.0-17ubuntu1) 12.3.0"
 	.section	.note.GNU-stack,"",@progbits
