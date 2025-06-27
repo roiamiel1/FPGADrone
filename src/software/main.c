8b34b77d56304e54045adcd8c769859ed046e763
Roi Amiel <roi@Rois-MacBook-Pro.local>
2025-06-28 00:18:56 +0300

    write hello to uart c code

--- src/software/main.c
+++ src/software/main.c
@@ -3,11 +3,11 @@
 #define P_UART_DONE ((char*) 1002)
 #define P_UART_BUSY ((char*) 1003)
 
-#define UART_PUTC(C)        \
-    while (*P_UART_BUSY);   \
-    *P_UART_CHAR = C;       \
-    *P_UART_START = 1;      \
-    while (!*P_UART_DONE);  \
+#define UART_PUTC(C)            \
+    while(*P_UART_BUSY != 0);   \
+    *P_UART_CHAR = C;           \
+    *P_UART_START = 1;          \
+    while (*P_UART_DONE == 0);  \
     *P_UART_START = 0;
 
 void main() {
@@ -17,13 +17,8 @@ void main() {
         UART_PUTC('l');
         UART_PUTC('l');
         UART_PUTC('o');
-        UART_PUTC(' ');
-        UART_PUTC('W');
-        UART_PUTC('o');
-        UART_PUTC('r');
-        UART_PUTC('l');
-        UART_PUTC('d');
         UART_PUTC('!');
+        UART_PUTC(' ');
     }
     return;
 }
