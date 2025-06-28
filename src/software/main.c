66e07e677f78addd5a661169ef06f31bf9a25f0b
Roi Amiel <roi@Rois-MacBook-Pro.local>
2025-06-28 20:05:42 +0300

    create test gowin_sp_4096_8 memory unit c program

--- src/software/main.c
+++ src/software/main.c
@@ -6,16 +6,27 @@
 #define UART_PUTC(C)            \
     while(*P_UART_BUSY != 0);   \
     *P_UART_CHAR = C;           \
-    *P_UART_START = 1;          \
+    *P_UART_START = 1; \
     while (*P_UART_DONE == 0);  \
     *P_UART_START = 0;
 
 void main() {
+    *((char*) 0) = 'H';
+    *((char*) 1) = 'e';
+    *((char*) 2) = 'l';
+    *((char*) 3) = 'l';
+    *((char*) 4) = 'o';
+    *((char*) 5) = '!';
+    *((char*) 6) = ' ';
+
     while (1) {
         UART_PUTC(*((char*) 0));
         UART_PUTC(*((char*) 1));
         UART_PUTC(*((char*) 2));
         UART_PUTC(*((char*) 3));
+        UART_PUTC(*((char*) 4));
+        UART_PUTC(*((char*) 5));
+        UART_PUTC(*((char*) 6));
     }
     return;
 }
