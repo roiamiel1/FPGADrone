ad889f146d402c50f758fcb5267d4ebb1000602a
Roi Amiel <roi@Rois-MacBook-Pro.local>
2025-12-28 23:34:48 +0200

    Update startup test c code

--- src/software/main.c
+++ src/software/main.c
@@ -1,28 +1,33 @@
-#define P_UART_CHAR ((char*) 1000)
-#define P_UART_START ((char*) 1001)
-#define P_UART_DONE ((char*) 1002)
-#define P_UART_BUSY ((char*) 1003)
+#define P_UART_CHAR  ((unsigned char*) 1000)
+#define P_UART_START ((unsigned char*) 1001)
+#define P_UART_DONE  ((unsigned char*) 1002)
+#define P_UART_BUSY  ((unsigned char*) 1003)
 
-#define UART_PUTC(C)            \
-    while(*P_UART_BUSY != 0);   \
-    *P_UART_CHAR = (const char) (C);    \
-    *P_UART_START = 1;          \
-    while (*P_UART_DONE == 0);  \
-    *P_UART_START = 0;
-
-// There is a problem in the simulation, that every time we check `i` value in the for loop, it returns 60 for some reason.
-// This is why we wont enter the for loop.
-// I think the problem is related to memory read or write, something about the timing with the clk or sample rate is wrong.
+#define UART_PUTC(C)                        \
+    do {                                    \   
+        while((*P_UART_BUSY) != 0) {}       \
+        *P_UART_CHAR = (unsigned char) (C); \
+        *P_UART_START = 1;                  \
+        while ((*P_UART_DONE) != 1) {}      \
+        *P_UART_START = 0;                  \   
+        while((*P_UART_BUSY) != 0) {}       \  
+    } while (0)
 
 __attribute__((section(".text")))
 void main() {
-    const char* msg = "0123012301230123012301230123012301230123012301230123012301230123";
-
-    while (1) {
-        UART_PUTC('Q');
-        for (char i = 0; i < 20; i++) {
-            UART_PUTC('A' + i);
-        }
+    char* msg = "abcdefghijklmnopqrstuvwxyz"; 
+    for (unsigned int i = 0; i < 26; i++) {
+        UART_PUTC(msg[i]);
+        msg[i + 1] = 'A' + i;
     }
+    /*
+    // while (1) {
+    UART_PUTC('0');
+    for (unsigned int i = 'A'; i < 'Z'; i++) {
+        UART_PUTC(i);
+    }
+    UART_PUTC('1');
+    // }
+    */
     return;
 }
