2d6db2e6f6159b1bdb3a8dbed1d75d474843aa9f
Roi Amiel <roi@Rois-MacBook-Pro.local>
2025-12-28 23:32:20 +0200

    GTKWave create alu op filter

---
+++ scripts/gtkwave_filter_alu_op.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+import sys
+import select
+import subprocess
+
+def main():
+    try:
+        while True:
+            readable, _, _ = select.select([sys.stdin], [], [], 0.1)
+            if not readable:
+                continue
+
+            l = sys.stdin.readline()
+            if not l:
+                raise EOFError()
+            
+            l = l.strip().lower()
+
+            if "x" in l:
+                sys.stdout.write("Undefined\n")
+                sys.stdout.flush()
+                continue
+
+            alu_opcode_map = {
+                "00": "ADD - A + B",
+                "01": "SUB - A - B",
+                "02": "MUL - A * B",
+                "03": "DIV - A / B",
+                "04": "SLL - A << 1",
+                "05": "SRL - A >> 1",
+                "06": "AND - A and B",
+                "07": "OR - A or B",
+                "08": "XOR - A xor B",
+                "09": "NOR - A nor B",
+                "0a": "NAND - A nand B",
+                "0b": "XNOR - A xnor B",
+                "0c": "BNE - Zero = 1 if A!=B else 0",
+                "0d": "SLT - 1 if A<B else 0",
+                "0e": "LUI - 1 if A<B else 0",
+                "0f": "IN1 - A",
+                "10": "SRA - A >> 1 (with sign extension)"
+            }
+
+            if l in alu_opcode_map:
+                sys.stdout.write(f"{alu_opcode_map[l]}\n")
+                sys.stdout.flush()
+                continue
+            else:
+                sys.stdout.write("Unknown\n")
+                sys.stdout.flush()
+                continue    
+
+    except Exception as e:
+        try:
+            sys.stdout.write("Unknown\n")
+            sys.stdout.flush()
+        except:
+            pass
+    except:
+        try:
+            sys.stdout.write("Unknown\n")
+            sys.stdout.flush()
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    main()
+    sys.exit(0)
