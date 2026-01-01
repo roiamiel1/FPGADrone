19ee665debff59fd833dcb3507061a81c33e5ab2
Roi Amiel <roi@Rois-MacBook-Pro.local>
2026-01-01 17:25:14 +0200

    make main.c startup test more complex with recursive and global static variable

--- src/software/main.c
+++ src/software/main.c
@@ -13,21 +13,55 @@
         while((*P_UART_BUSY) != 0) {}       \  
     } while (0)
 
+static int make_upper = 1;
+
+char recursive_func(int index) {
+    if (index <= 0) {
+        return '!';
+    } else {
+        return recursive_func(index - 1);
+    }
+}
+
+char func(char* msg, unsigned int len) {
+    for (unsigned int i = 0; i < len && make_upper; i++) {
+        if (msg[i] == ' ' || msg[i] == '\n') {
+            continue;
+        }
+
+        msg[i] += ('A' - 'a');
+    }
+
+    for (unsigned int i = 0; i < len; i++) {
+        UART_PUTC(msg[i]);
+    }
+
+    for (unsigned int i = 0; i < len && make_upper; i++) {
+        if (msg[i] == ' ' || msg[i] == '\n') {
+            continue;
+        }
+
+        msg[i] -= ('A' - 'a');
+    }
+
+    if (make_upper == 0) {
+        make_upper = 1;
+    } else {
+        make_upper = 0;
+    }
+
+    UART_PUTC(recursive_func(5));
+
+    return '\n';
+}
+
 __attribute__((section(".text")))
 void main() {
-    char* msg = "abcdefghijklmnopqrstuvwxyz"; 
-    for (unsigned int i = 0; i < 26; i++) {
-        UART_PUTC(msg[i]);
-        msg[i + 1] = 'A' + i;
+    char msg[] = "hello my name is roi"; 
+
+    while (1) {
+        UART_PUTC(func(msg, sizeof(msg) - 1));
     }
-    /*
-    // while (1) {
-    UART_PUTC('0');
-    for (unsigned int i = 'A'; i < 'Z'; i++) {
-        UART_PUTC(i);
-    }
-    UART_PUTC('1');
-    // }
-    */
+
     return;
 }
