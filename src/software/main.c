4145e121c182f6b23248ed13137110f96b7ed8e8
Roi Amiel <roi@Rois-MacBook-Pro.local>
2026-01-02 19:11:41 +0200

    main.c add esc DMA access interface to drive 1 motor

--- src/software/main.c
+++ src/software/main.c
@@ -1,65 +1,46 @@
-#define P_UART_CHAR  ((unsigned char*) 1000)
-#define P_UART_START ((unsigned char*) 1001)
-#define P_UART_DONE  ((unsigned char*) 1002)
-#define P_UART_BUSY  ((unsigned char*) 1003)
+#define P_UART_CHAR  ((unsigned char*) 0xFFFFFFFF)
+#define P_UART_START ((unsigned char*) 0xFFFFFFFE)
+#define P_UART_DONE  ((unsigned char*) 0xFFFFFFFD)
+#define P_UART_BUSY  ((unsigned char*) 0xFFFFFFFC)
+#define P_ESC1_SPEED ((unsigned int*)  0xFFFFFFFB)
+
+#define SET_ESC1_SPEED(S)                   \
+    do {                                    \
+        *P_ESC1_SPEED = (unsigned int) (S); \
+    } while (0)
+
+#define GET_ESC1_SPEED() (*P_ESC1_SPEED)
 
 #define UART_PUTC(C)                        \
-    do {                                    \   
+    do {                                    \
+        *P_UART_START = 0;                  \
         while((*P_UART_BUSY) != 0) {}       \
         *P_UART_CHAR = (unsigned char) (C); \
         *P_UART_START = 1;                  \
-        while ((*P_UART_DONE) != 1) {}      \
-        *P_UART_START = 0;                  \   
-        while((*P_UART_BUSY) != 0) {}       \  
+        while ((*P_UART_DONE) == 0) {}      \
+        *P_UART_START = 0;                  \
+        while((*P_UART_BUSY) != 0) {}       \
+        while ((*P_UART_DONE) != 0) {}      \
     } while (0)
 
-static int make_upper = 1;
 
-char recursive_func(int index) {
-    if (index <= 0) {
-        return '!';
-    } else {
-        return recursive_func(index - 1);
-    }
+int printf(const char *restrict fmt, ...) {
+	int ret;
+	va_list ap;
+	va_start(ap, fmt);
+	ret = vfprintf(stdout, fmt, ap);
+	va_end(ap);
+	return ret;
 }
 
-char func(char* msg, unsigned int len) {
-    for (unsigned int i = 0; i < len && make_upper; i++) {
-        if (msg[i] == ' ' || msg[i] == '\n') {
-            continue;
-        }
-
-        msg[i] += ('A' - 'a');
-    }
-
-    for (unsigned int i = 0; i < len; i++) {
-        UART_PUTC(msg[i]);
-    }
-
-    for (unsigned int i = 0; i < len && make_upper; i++) {
-        if (msg[i] == ' ' || msg[i] == '\n') {
-            continue;
-        }
-
-        msg[i] -= ('A' - 'a');
-    }
-
-    if (make_upper == 0) {
-        make_upper = 1;
-    } else {
-        make_upper = 0;
-    }
-
-    UART_PUTC(recursive_func(5));
-
-    return '\n';
-}
 
 __attribute__((section(".text")))
 void main() {
     char msg[] = "hello my name is roi"; 
 
     while (1) {
+        SET_ESC1_SPEED(800);
+        
         UART_PUTC(func(msg, sizeof(msg) - 1));
     }
 
